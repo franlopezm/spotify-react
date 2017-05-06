d418c584a007f8d5706a74e49b451fd97ef0af68
FransLopez <frans.lopez.m@gmail.com>
2017-05-06 18:51:27 +0200

    fix change

--- src/App.js
+++ src/App.js
@@ -1,69 +1,61 @@
-import React, { Component } from 'react'
-import './App.css'
+import React, { Component } from 'react';
+import './App.css';
 
-import SearchArtist from './components/SearchArtist'
-import ListArtists from './components/ListArtists'
-import ListAlbums from './components/ListAlbums'
-import ListTracks from './components/ListTracks'
+import SearchArtist from './components/SearchArtist';
+import ListArtists from './components/ListArtists';
+import ListAlbums from './components/ListAlbums';
+import ListTracks from './components/ListTracks';
 
-class App extends Component {
-  constructor () {
-    super()
+export default class App extends Component {
+  constructor() {
+    super();
+
     this.state = {
       artists: [],
       albums: [],
-      tracks: []
-    }
+      tracks: [],
+    };
+
+    this.searchArtist = this.handleSearchArtist.bind(this);
+    this.selectArtist = this.handleSelectArtist.bind(this);
+    this.selectAlbum = this.handleSelectAlbum.bind(this);
   }
 
-  handleSearchArtist (event) {
-    event.preventDefault()
-    let artist = event.target.artist.value
+// Handler Functions
+  handleSearchArtist(event) {
+    event.preventDefault();
+    const artist = event.target.artist.value;
     fetch(`https://api.spotify.com/v1/search?type=artist&query=${artist}`)
-      .then(response => {
-        return response.json()
-      })
-      .then(elem => {
-        this.setState({ artists: elem.artists.items })
-      })
+      .then(response => response.json())
+      .then(elem => this.setState({ artists: elem.artists.items }));
   }
 
-  handleSelectArtist (event) {
-    let idAlbums = event.target.value
+  handleSelectArtist(event) {
+    const idAlbums = event.target.value;
     fetch(`https://api.spotify.com/v1/artists/${idAlbums}/albums`)
-      .then(response => {
-        return response.json()
-      })
-      .then(elem => {
-        this.setState({ albums: elem.items })
-      })
+      .then(response => response.json())
+      .then(elem => this.setState({ albums: elem.items }));
   }
 
-  handleSelectAlbum (event) {
-    let idAlbum = event.target.value
+  handleSelectAlbum(event) {
+    const idAlbum = event.target.value;
     fetch(`https://api.spotify.com/v1/albums/${idAlbum}/tracks`)
-      .then(response => {
-        return response.json()
-      })
-      .then(elem => {
-        console.log(elem.items)
-        this.setState({ tracks: elem.items })
-      })
+      .then(response => response.json())
+      .then(elem => this.setState({ tracks: elem.items }));
   }
 
 
-  render () {
+// Render
+  render() {
     return (
-      <div className='container'>
-        <div className='jumbotron'>
-          <SearchArtist searchArtist={this.handleSearchArtist.bind(this)} />
-          <ListArtists artists={this.state.artists} selectArtist={this.handleSelectArtist.bind(this)} />
-          <ListAlbums albums={this.state.albums} selectAlbum={this.handleSelectAlbum.bind(this)} />
+      <div className="container">
+        <div className="jumbotron">
+          <SearchArtist searchArtist={this.searchArtist} />
+          <ListArtists artists={this.state.artists} selectArtist={this.selectArtist} />
+          <ListAlbums albums={this.state.albums} selectAlbum={this.selectAlbum} />
           <ListTracks tracks={this.state.tracks} />
         </div>
       </div>
-    )
+    );
   }
 }
-
-export default App
--- src/components/Track.js
+++ src/components/Track.js
@@ -1,14 +1,12 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
 
-class Track extends Component {
-  render () {
+export default class Track extends Component {
+  render() {
     return (
       <li>
-          <audio src={this.props.track.preview_url} type="audio/mpeg" controls></audio>
-          <p>{this.props.track.name}</p>
+        <audio src={this.props.track.preview_url} type="audio/mpeg" controls></audio>
+        <p>{this.props.track.name}</p>
       </li>
     )
   }
 }
-
-export default Track
