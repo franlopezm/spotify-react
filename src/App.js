413a3e6c09ffbc1735221c5999be385a58e238bf
Francisco Lopez Muñoz <frans.lopez.m@gmail.com>
2017-12-28 14:21:31 +0100

    Moved calls to the api a dataService.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import DataService from './services/dataService';
+import urlHelpers from './helpers/urlHelpers';
+
 import SearchArtist from './components/SearchArtist';
 import ListArtists from './components/ListArtists';
 import ListAlbums from './components/ListAlbums';
 import ListTracks from './components/ListTracks';
 
+
 export default class App extends Component {
-  constructor() {
-    super();
+  constructor (props) {
+    super (props);
 
     this.state = {
       artists: [],
@@ -21,38 +25,46 @@ export default class App extends Component {
     this.selectAlbum = this.handleSelectAlbum.bind(this);
   }
 
+  componentWillMount () {
+    let hashParams = urlHelpers.getHashParams();
+
+    if (!hashParams.access_token) {
+      DataService.getToken();
+
+    } else {
+      DataService.addToken(hashParams.access_token);
+    }
+  }
+
 // Handler Functions
-  handleSearchArtist(event) {
+  handleSearchArtist (event) {
     event.preventDefault();
-    const artist = event.target.artist.value;
-    fetch(`https://api.spotify.com/v1/search?type=artist&query=${artist}`)
-      .then(response => response.json())
+    
+    DataService.searchArtist(event)
       .then(elem => this.setState({ artists: elem.artists.items }));
   }
 
-  handleSelectArtist(event) {
-    const idAlbums = event.target.value;
-    fetch(`https://api.spotify.com/v1/artists/${idAlbums}/albums`)
-      .then(response => response.json())
+  handleSelectArtist (event) {
+    DataService.selectArtist(event)
       .then(elem => this.setState({ albums: elem.items }));
   }
 
-  handleSelectAlbum(event) {
-    const idAlbum = event.target.value;
-    fetch(`https://api.spotify.com/v1/albums/${idAlbum}/tracks`)
-      .then(response => response.json())
+  handleSelectAlbum (event) {
+    DataService.selectAlbum(event)
       .then(elem => this.setState({ tracks: elem.items }));
   }
 
 
-// Render
-  render() {
+  render () {
     return (
       <div className="container">
         <div className="jumbotron">
           <SearchArtist searchArtist={this.searchArtist} />
+
           <ListArtists artists={this.state.artists} selectArtist={this.selectArtist} />
+          
           <ListAlbums albums={this.state.albums} selectAlbum={this.selectAlbum} />
+          
           <ListTracks tracks={this.state.tracks} />
         </div>
       </div>
