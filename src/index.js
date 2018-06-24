da39298ce6cb4709fb9da168f27fe038346fb889
Francisco Lopez Muñoz <frans.lopez.m@gmail.com>
2018-06-24 21:34:27 +0200

    Added images, changed index.html, manifest and package.json

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
 
 import App from './components/App';
 
@@ -9,3 +10,5 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+registerServiceWorker();
