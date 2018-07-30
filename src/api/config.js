94292e5a3742b637b7b14733e9fcc3439f623d79
Francisco Lopez Muñoz <frans.lopez.m@gmail.com>
2018-07-30 18:00:50 +0200

    Added config in api

---
+++ src/api/config.js
@@ -0,0 +1,9 @@
+export default {
+  urlBase: 'https://api.spotify.com/v1/',
+  urlAuth: 'https://accounts.spotify.com/authorize',
+  clientId: '54ceda5e055145f689fef81d80325eda',
+  redirectUri: window.location.origin,
+  responseType: 'token',
+  scope: '',
+  showDialog: true
+};
